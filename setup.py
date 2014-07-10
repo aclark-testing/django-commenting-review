5810375ce2a0808c932c036847183ac4a87dc28a
Alex Clark <aclark@aclark.net>
2014-07-10 18:16:15 -0400

    Add files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    entry_points={
+    },
+    include_package_data=True,
+    install_requires=[
+        'Django',
+    ],
+    name='django-commenting-review',
+    namespace_packages=[
+    ],
+    packages=find_packages(),
+)
